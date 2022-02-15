f828a5ae64035356fe56a057b6483f199e7a046d
Roy <J.jyun0525@gmail.com>
2022-02-15 16:42:24 +0900

    어댑터 변수 추가 , 리스트 뷰의 어댑터로써 연결

--- app/src/main/java/com/example/listviewpractice_ex_yun/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_ex_yun/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.listviewpractice_ex_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_ex_yun.adapters.FriendsAdapter
 import com.example.listviewpractice_ex_yun.datas.Friends
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mFriendsList = ArrayList<Friends>()
+
+    lateinit var mAdapter : FriendsAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +24,15 @@ class MainActivity : AppCompatActivity() {
         mFriendsList.add(Friends("정영오", "010-0505-0505", 2005))
         mFriendsList.add(Friends("조영육", "010-0606-0606", 2006))
         mFriendsList.add(Friends("곽영칠", "010-0707-0707", 2007))
+
+//        만들어둔 어댑터를 리스트와 액티비티에서 연결 위에서 멤버변수 먼저 만들기
+//        리스트를 다 더해서 채운 다음에 실행하기 때문에 lateinit var mAdapter
+//        이퀄이 아니라 나중에 넣을 거라 :해줌.
+
+//        어댑터 클래스를 객체화
+//        어댑터 변수 = 어댑터클래스(this, R.layout.~list_item, 목록변수 이름)
+        mAdapter = FriendsAdapter(this,R.layout.friend_list_item, mFriendsList)
+
+        friendsListView.adapter = mAdapter
     }
 }
