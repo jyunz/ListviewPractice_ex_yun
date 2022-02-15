a694d3ed0a4cf479725bdd8913c519c0443f7ca9
Roy <J.jyun0525@gmail.com>
2022-02-15 16:31:26 +0900

    실제 목록 추가

--- app/src/main/java/com/example/listviewpractice_ex_yun/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_ex_yun/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.listviewpractice_ex_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_ex_yun.datas.Friends
 
 class MainActivity : AppCompatActivity() {
+
+    val mFriendsList = ArrayList<Friends>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFriendsList.add(Friends("김영일", "010-0101-0101", 2001))
+        mFriendsList.add(Friends("박영이", "010-0202-0202", 2002))
+        mFriendsList.add(Friends("이영삼", "010-0303-0303", 2003))
+        mFriendsList.add(Friends("최영사", "010-0404-0404", 2004))
+        mFriendsList.add(Friends("정영오", "010-0505-0505", 2005))
+        mFriendsList.add(Friends("조영육", "010-0606-0606", 2006))
+        mFriendsList.add(Friends("곽영칠", "010-0707-0707", 2007))
     }
 }
--- app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
@@ -8,26 +8,23 @@ import android.widget.ArrayAdapter
 import com.example.listviewpractice_ex_yun.R
 import com.example.listviewpractice_ex_yun.datas.Friends
 
-class FriendsAdapter(
-    val mContext : Context,
-    val resId : Int,
-    val mList : ArrayList<Friends>) : ArrayAdapter<Friends> (mContext,resId,mList) {
-//      ㄴ 어댑터의 주 생성자에서 필요한 재료를 받자
-//      스튜던트 리스트 아이템과 스튜던트 클래스를 결합하는 것을 여기서 하는것.멤버변수로 생성
+class FriendsAdapter(val mContext : Context, val resId : Int, val mList : ArrayList<Friends>) : ArrayAdapter<Friends> (mContext,resId,mList) {
 
     val inf = LayoutInflater.from(mContext)
 
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-//    getView는 convertView라는 변수를 가지고 와서 실행하기 때문에 convertView를 검사를 한번 해줌
-//    그래서 검사를 위한 내용이 들어감,
+
         var tempRow = convertView
         if (tempRow == null) {
-            tempRow = inf.inflate(R.layout.friend_list_item,null)
-//            null->부가 정보가 아직 없다는 뜻
+            tempRow = inf.inflate(R.layout.friend_list_item, null)
+
         }
 
         val row = tempRow!!
-//        위에서 tempRow가 비어있으면 채우라고 했기때문에 비어있을 수 없다는 뜻으로 !!
+
+        return row
+
     }
+
+
 }
