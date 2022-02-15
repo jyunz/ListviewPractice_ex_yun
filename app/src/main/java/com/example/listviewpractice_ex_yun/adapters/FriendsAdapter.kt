8a422962658aa5d39029c065fbb7f8a3b7d35d8e
Roy <J.jyun0525@gmail.com>
2022-02-15 15:49:59 +0900

    목록을 뿌려주는 담당인, Adapter 추가작업 - ArrayAdapter를 물려받아서 기능활용 예정
    flater(=>inf)
    - xml을 가지고 객체로 변환해쥬는 변수를 멤버 변수로 생성

--- app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.listviewpractice_ex_yun.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
 import com.example.listviewpractice_ex_yun.datas.Friends
 
@@ -8,6 +9,8 @@ class FriendsAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Friends>) : ArrayAdapter<Friends> (mContext,resId,mList){
-    
+//      ㄴ 어댑터의 주 생성자에서 필요한 재료를 받자
+//      스튜던트 리스트 아이템과 스튜던트 클래스를 결합하는 것을 여기서 하는것.멤버변수로 생성
 
-}
+        val inf = LayoutInflater.from(mContext)
+    }
