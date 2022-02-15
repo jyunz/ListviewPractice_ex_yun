024c0fe9458ff0ae427f80ff1a6c075f8107249d
Roy <J.jyun0525@gmail.com>
2022-02-15 15:55:05 +0900

    tempRow는 convertView를 검사를 위한 임시로 쓰는 한칸 이라는 뜻으로 네이밍한 것

--- app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_ex_yun/adapters/FriendsAdapter.kt
@@ -2,6 +2,8 @@ package com.example.listviewpractice_ex_yun.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.listviewpractice_ex_yun.datas.Friends
 
@@ -12,5 +14,14 @@ class FriendsAdapter(
 //      ㄴ 어댑터의 주 생성자에서 필요한 재료를 받자
 //      스튜던트 리스트 아이템과 스튜던트 클래스를 결합하는 것을 여기서 하는것.멤버변수로 생성
 
-        val inf = LayoutInflater.from(mContext)
+    val inf = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//    getView는 convertView라는 변수를 가지고 와서 실행하기 때문에 convertView를 검사를 한번 해줌
+//    그래서 검사를 위한 내용이 들어감,
+
+        var tempRow = convertView
+        if (tempRow == null) {
     }
+}
